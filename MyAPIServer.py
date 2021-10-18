15e112fc8c7306c28182b1c0ff8a7ae57b0b17a2
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-10-18 20:18:46 +0800

    update

--- MyAPIServer.py
+++ MyAPIServer.py
@@ -3,9 +3,13 @@ from flask import request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Base"
+
 @app.route('/GoogleDriveAPI/')
 def GoogleDriveAPIFun():
-    return "Hello"
+    return "Google Drive API"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
