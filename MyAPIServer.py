b1f616ff715d3a10159c5c53b0336796959a9244
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-10-19 22:36:45 +0800

    update

--- MyAPIServer.py
+++ MyAPIServer.py
@@ -52,7 +52,7 @@ def GDA_UpdateRegularImage():
 db_settings = {
     "host": "127.0.0.1",
     "port": 3306,
-    "user": "alan",
+    "user": "root",
     "password": "12345678",
     "db": "gd_db",
     "charset": "utf8"
