dd2c02362f538b70564a30036c3f5c235d6ad828
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-10-19 10:01:37 +0800

    update

--- MyAPIServer.py
+++ MyAPIServer.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +9,15 @@ def index():
 @app.route('/GoogleDriveAPI/')
 def GoogleDriveAPIFun():
     return "Google Drive API"
+
+@app.route('/GoogleDriveAPI/UpdateRegularImage')
+def GDA_UpdateRegularImage():
+    print('Update Regular Image')
+
+    res = {}
+    res['result']='Success'
+    res['errcode']=''
+    return jsonify(res)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
