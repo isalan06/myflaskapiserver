6a52394b2d4d14ce97a089551f0bd9168dbdc9b5
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-11-08 10:02:42 +0800

    update

--- MyAPIServer.py
+++ MyAPIServer.py
@@ -21,6 +21,70 @@ def index():
 def GoogleDriveAPIFun():
     return "Google Drive API"
 
+
+@app.route('/GoogleDriveAPI/DeleteImage', methods=['GET'])
+def GDA_DeleteFiles():
+    print('Delete Files')
+    _sn = request.args['sn']
+
+    image_code = getUpdateGDImageCode(_sn)
+    if image_code != '':
+        try:
+            cred_folderString = os.path.join(os.sep, os.path.abspath(os.path.dirname(__file__)), 'iotgwgoogledrive')
+            cred_filenameString = os.path.join(os.sep, cred_folderString, 'token.json')
+            clientcred_filenameString = os.path.join(os.sep, cred_folderString ,'client_secrets.json')
+
+            creds = None
+
+            if os.path.exists(cred_filenameString):
+                #print("GD1")
+                creds = Credentials.from_authorized_user_file(cred_filenameString, SCOPES)
+            # If there are no (valid) credentials available, let the user log in.
+            if not creds or not creds.valid:
+                #print("GD2")
+                if creds and creds.expired and creds.refresh_token:
+                    #print("GD3")
+                    creds.refresh(Request())
+                else:
+                    #print("GD4")
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        clientcred_filenameString, SCOPES)
+                    creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open(cred_filenameString, 'w') as token:
+                    token.write(creds.to_json())
+
+            service = build('drive', 'v3', credentials=creds)
+
+            folder_id = image_code
+
+            page_token = None
+            while True:
+                response = drive_service.files().list(q="mimeType='image/jpeg' and '" + folder_id + "' in parents",
+                                          spaces='drive',
+                                          fields='nextPageToken, files(id, name)',
+                                          pageToken=page_token).execute()
+                for file in response.get('files', []):
+                    # Process change
+                    print 'Found file: %s (%s)' % (file.get('name'), file.get('id'))
+                page_token = response.get('nextPageToken', None)
+                if page_token is None:
+                    break
+
+            print('Delete Image From Google Drive Success')
+
+        except Exception as ex:
+            print(ex)
+            res2 = {}
+            res2['result']='failure'
+            res2['errcode']='Delete Image From Google Drive happen error:' + str(ex)
+            return jsonify(res2)
+    res = {}
+    res['result']='success'
+    res['errcode']=''
+    return jsonify(res)
+
+
 @app.route('/GoogleDriveAPI/UpdateRegularImage', methods=['POST'])
 def GDA_UpdateRegularImage():
     print('Update Regular Image')
